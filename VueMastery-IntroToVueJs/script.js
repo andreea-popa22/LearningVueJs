748bf8549b55d8d446be7a4b0632f3098ce76fc7
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-05 10:22:17 +0300

    initial commit

---
+++ VueMastery-IntroToVueJs/script.js
@@ -0,0 +1,8 @@
+//Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks'
+  } 
+})
