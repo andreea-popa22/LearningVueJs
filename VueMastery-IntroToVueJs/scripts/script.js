f50b7dfd4d23ea2de6cdc81e5ce5d1945442c8f5
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-05 10:59:24 +0300

    attribute binding

--- VueMastery-IntroToVueJs/scripts/script.js
+++ VueMastery-IntroToVueJs/scripts/script.js
@@ -4,6 +4,8 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks'
+    description: 'A pair of warm, fuzzy socks',
+    image: './resources/socks.jpg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
   } 
 })
