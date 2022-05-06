889f1cb577b4fe61fd92c0aec48ae3de71ffec08
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-06 13:14:34 +0300

    computed properties

--- VueMastery-IntroToVueJs/scripts/script.js
+++ VueMastery-IntroToVueJs/scripts/script.js
@@ -4,23 +4,27 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
+    brand: 'NDR Style',
     description: 'A pair of warm, fuzzy socks',
-    image: './resources/green-socks.jpg',
-    inStock: false,
+    // image: './resources/green-socks.jpg',
+    selectedVariant: 0,
+    // inStock: false,
     // inventory: 9,
-    // onSale: true,
+    onSale: true,
     // link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
     details: ["85% cotton", "15% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 1,
         variantColor: "Green",
-        variantImage: './resources/green-socks.jpg'
+        variantImage: './resources/green-socks.jpg',
+        variantQuantity: 10
       },
       {
         variantId: 2,
         variantColor: "Blue",
-        variantImage: './resources/blue-socks.jpg'
+        variantImage: './resources/blue-socks.jpg',
+        variantQuantity: 0
       }
     ],
     sizes: ['35-38', '39-41', '42-45'],
@@ -35,8 +39,25 @@ var app = new Vue({
         this.cart -= 1
       }
     },
-    updateProduct: function(variantImage) {
-      this.image = variantImage
+    updateProduct: function(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() { 
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on sale!'
+      } 
+        return  this.brand + ' ' + this.product + ' are not on sale'
     }
   }
 })
