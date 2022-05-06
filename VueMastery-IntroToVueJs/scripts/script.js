80f4b7cbe555c0479493fc64af1e9a80b427999e
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-06 13:38:37 +0300

    components

--- VueMastery-IntroToVueJs/scripts/script.js
+++ VueMastery-IntroToVueJs/scripts/script.js
@@ -1,34 +1,96 @@
-//Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+})
 
-var app = new Vue({
-  el: '#app',
-  data: {
-    product: 'Socks',
-    brand: 'NDR Style',
-    description: 'A pair of warm, fuzzy socks',
-    // image: './resources/green-socks.jpg',
-    selectedVariant: 0,
-    // inStock: false,
-    // inventory: 9,
-    onSale: true,
-    // link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
-    details: ["85% cotton", "15% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 1,
-        variantColor: "Green",
-        variantImage: './resources/green-socks.jpg',
-        variantQuantity: 10
-      },
-      {
-        variantId: 2,
-        variantColor: "Blue",
-        variantImage: './resources/blue-socks.jpg',
-        variantQuantity: 0
+Vue.component('product', {
+  props: {
+      premium: {
+        type: Boolean,
+        required: true
       }
-    ],
-    sizes: ['35-38', '39-41', '42-45'],
-    cart: 0
+  },
+  template: `
+  <div class="product">
+    <div class="product-image">
+      <img :src="image" />
+    </div>
+
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+      <p v-if="inStock">In Stock</p>
+      <p v-else :class="{ outOfStock: !inStock }">Out Of Stock</p>
+      <p>User is premium: {{ premium }}</p>
+      <p>Shipping: {{ shipping }}</p>
+      <p>{{ sale }}</p>
+      <p>Details:</p>
+      <product-details :details="details"></product-details>
+      <p>Available colors:</p>
+      <div v-for="(variant, index) in variants" 
+            :key="variant.variantId"
+            class="color-box"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(index)">
+    </div>
+    <p>Sizes:</p>
+    <ul>
+      <li v-for="size in sizes">{{ size }}</li>
+    </ul>
+
+    <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }">
+            Add to Cart
+    </button>
+    <button @click="removeFromCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }">
+            Remove from Cart
+    </button>
+    <div class="cart">
+      <p>Cart({{cart}})</p>
+    </div>
+  </div>
+  `,
+  data() {
+    return {
+      product: 'Socks',
+      brand: 'NDR Style',
+      description: 'A pair of warm, fuzzy socks',
+      // image: './resources/green-socks.jpg',
+      selectedVariant: 0,
+      // inStock: false,
+      // inventory: 9,
+      onSale: true,
+      // link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+      details: ["85% cotton", "15% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 1,
+          variantColor: "Green",
+          variantImage: './resources/green-socks.jpg',
+          variantQuantity: 10
+        },
+        {
+          variantId: 2,
+          variantColor: "Blue",
+          variantImage: './resources/blue-socks.jpg',
+          variantQuantity: 0
+        }
+      ],
+      sizes: ['35-38', '39-41', '42-45'],
+      cart: 0
+    }
   },
   methods: {
     addToCart: function() {
@@ -58,6 +120,21 @@ var app = new Vue({
         return this.brand + ' ' + this.product + ' are on sale!'
       } 
         return  this.brand + ' ' + this.product + ' are not on sale'
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free"
+      }
+      else {
+        return "2.99$"
+      }
     }
+  }
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: false
   }
 })
