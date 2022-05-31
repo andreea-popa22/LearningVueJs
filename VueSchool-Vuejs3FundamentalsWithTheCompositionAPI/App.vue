a23b09eda4c83a2c0ae27d53c6b590fb7aabdf2c
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-31 12:24:29 +0300

    conditionals

--- VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
+++ VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
@@ -2,10 +2,11 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
+const editing = ref(false)
 const items = ref([
-  {id: 1, label: "10 party hats"},
-  {id: 2, label:"2 board games"},
-  {id: 3, label: "20 cups"}
+  // {id: 1, label: "10 party hats"},
+  // {id: 2, label:"2 board games"},
+  // {id: 3, label: "20 cups"}
 ])
 const newItem = ref("")
 const newItemHighPriority = ref(false)
@@ -13,12 +14,25 @@ const saveItem = ()=>{
 	items.value.push({id: items.value.length + 1,label: newItem.value})
   newItem.value = ""
 }
+const doEdit = (e)=>{
+  editing.value = e
+  newItem.value = ""
+}
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
+  <div class="header">
+  	<h1>{{ header }}</h1>
+    <button v-if="editing" class="btn" @click="doEdit(false)">
+      Cancel
+    </button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">
+      Add Item
+    </button>
+  </div>
   <form 
 		class="add-item-form"
+    v-if="editing"
   	@submit.prevent="saveItem"       
 	>
     <input 
@@ -41,4 +55,8 @@ const saveItem = ()=>{
       {{label}}
     </li>
   </ul>
+  <p v-if="!items.length">
+    Nothing to see here
+  </p>
+  
 </template>
