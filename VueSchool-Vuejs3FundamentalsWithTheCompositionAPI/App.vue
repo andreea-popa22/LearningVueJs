0440640700a8a941f553f0ac3e2f0f91a234319a
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-31 12:10:00 +0300

    user inputs

--- VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
+++ VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
@@ -3,15 +3,25 @@ import { ref } from 'vue'
 
 const header = ref('Shopping List App')
 const items = ref([
-  {id:"1", label: "10 hats"},
-  {id:"2", label: "2 boardgames"},
-  {id:"3", label: "15 cups"}
+  {id: 1, label: "10 party hats"},
+  {id: 2, label:"2 board games"},
+  {id: 3, label: "20 cups"}
 ])
+const newItem = ref("")
+const newItemHighPriority = ref(false)
+const iceCreamFlavors = ref([])
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Add an item">
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority"> 
+    High Priority
+  </label>
   <ul>
-    <li v-for="({id, label}, index) in items" :key="id">{{label}}</li>
+    <li v-for="({id, label}, index) in items" :key="id">
+      {{label}}
+    </li>
   </ul>
 </template>
