d99dfde7f6e0f46128f28db6afecabcad3fe331b
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-31 11:43:42 +0300

    template syntax and expressions

--- VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
+++ VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
@@ -1,10 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 
-const msg = ref('Hello World!')
+const header = ref('Shopping List App')
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <input v-model="msg">
+  <h1>{{ header }}</h1>
 </template>
