9327e6ef1b87413dd115ccc8afcb922597a042be
Andreea Popa <andreea_popa22@yahoo.ro>
2022-05-31 12:02:52 +0300

    list rendering

--- VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
+++ VueSchool-Vuejs3FundamentalsWithTheCompositionAPI/App.vue
@@ -2,8 +2,16 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
+const items = ref([
+  {id:"1", label: "10 hats"},
+  {id:"2", label: "2 boardgames"},
+  {id:"3", label: "15 cups"}
+])
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <ul>
+    <li v-for="({id, label}, index) in items" :key="id">{{label}}</li>
+  </ul>
 </template>
